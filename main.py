92083334449835ae9ac284ab625690c818cc7eb0
Pscodium <peter-larson@hotmail.com>
2022-01-17 11:23:42 -0300

    feat(#3): interface duplicada em dois frames

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ color5 = '#E0E89E'
 
 
 window = tk.Tk()
-window.geometry('200x200')
+window.geometry('300x300')
 window.title('Python Project')
 window.resizable(False, False)
 window.config(bg=color1)
+
+
+up_frame = Frame(window, width=300, height=50, bg=color4, relief='flat')
+up_frame.grid(row=0, column=0, sticky=NSEW)
+
+down_frame = Frame(window, width=300, height=250, bg=color1, relief='flat')
+down_frame.grid(row=1, column=0, sticky=NSEW)
+
 
 window.mainloop()
