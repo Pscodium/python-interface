25d506bb820ee4a8429964786d67d58b404c6a8d
Pscodium <peter-larson@hotmail.com>
2022-01-17 11:14:00 -0300

    feat(#2): adicionando cor ao background

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 from tkinter import *
 
+### colors
+color1 = '#A5F2C0'
+color2 = '#DE9EE8'
+color3 = '#DBB99F'
+color4 = '#B0BCF5'
+color5 = '#E0E89E'
+
 
 window = tk.Tk()
 window.geometry('200x200')
 window.title('Python Project')
 window.resizable(False, False)
+window.config(bg=color1)
 
 window.mainloop()
